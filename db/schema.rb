dc701d2f1af342223781a5e57ae2be2f0cdf195b
Setiadi <setiadi.seth@gmail.com>
2023-07-11 00:48:02 +0700

    Add style and structure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_154309) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_165705) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_154309) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
